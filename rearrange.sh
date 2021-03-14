981e8cfca8f5ac59365776e0445771854a8b2db4
Nicolas <toux24@gmail.com>
2021-03-14 14:43:20 +0100

    First commit A quick start of the .sh and a beginning of the "how_to.txt" file Some musics for tests purposes

---
+++ rearrange.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+music_folder="Music"
+
+if [ -d $music_folder ]
+then
+    cd $music_folder
+    for folder in */ ; do
+	cd $folder
+	for music in * ; do
+	    mp3info $music
+	done
+	cd ..
+    done
+else
+    echo "Error: this directory does not exist."
+    echo "Press M to print the manual"
+    read -n1 key
+    echo
+    if [ $key == "m" ] || [ $key == "M" ]; then echo && cat how_to.txt; fi;
+fi
